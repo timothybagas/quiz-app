0703eb3472c75eab3a69363fb9598299320571e8
timothybagas <63138796+timothybagas@users.noreply.github.com>
2022-11-30 15:31:57 +0700

    Add a middleware

--- main.go
+++ main.go
@@ -11,6 +11,19 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.Use(func(context *gin.Context) {
+		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, DELETE")
+
+		if context.Request.Method == "OPTIONS" {
+			context.AbortWithStatus(204)
+			return
+		}
+		context.Next()
+	})
+
 	// returns a list of quizzes
 	r.GET("/quizzes", logics.GetQuizzes)
 	// returns a random quiz
